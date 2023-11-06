6f978a389a92354b7ac39432a6bcef54ebf437e4
suraj <Surajlsawant001@gmail.com>
2023-11-06 16:30:35 +0530

    RRS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter } from 'react-router-dom';
+import Base from './components/Base';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Menu from './pages/Menu';
+import { Route, Router, Routes } from 'react-router';
+import Booking from './pages/Booking';
+import { ToastContainer } from 'react-toastify';
+import Home from './pages/Home'
+import About from './pages/About';
+import OwnerLogin from './pages/OwnerLogin';
+import OwnerDashboard from './pages/OwnerDashboard';
+import Contact from './pages/Contact';
+import BookingDetails from './pages/BookingDetails';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+  
+  <BrowserRouter>
+  <ToastContainer />
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/signup" element={<Signup />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/dashboard" element={<Dashboard />}></Route>
+      <Route path="/menu" element={<Menu />}></Route>
+      <Route path="/booking" element={<Booking />}></Route>
+      <Route path="/ownerlogin" element={<OwnerLogin/>}></Route>
+      <Route path="/ownerdashboard" element={<OwnerDashboard/>}></Route>
+      <Route path="/contact" element={<Contact/>}></Route>
+      <Route path="/bookingdetails" element={<BookingDetails />}/>
+
+     
+    </Routes>
+  </BrowserRouter>
+    </>
   );
 }
 
